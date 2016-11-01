726e2003dd6d6ca99088b164efaf6350f93ab287
NaThAn H <stuff@whatever.com>
2016-11-01 14:03:29 -0700

    a

--- app/src/main/java/com/academy/app/courtcounter/MainActivity.java
+++ app/src/main/java/com/academy/app/courtcounter/MainActivity.java
@@ -2,12 +2,25 @@ package com.academy.app.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView point1View;
+    private int point1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        point1View = (TextView)findViewById(R.id.addpoint1);
+        point1=0;
+        point1View.setText("0");
+        }
+
+    public void clickpoint1(View view){
+        point1++;
+        point1View.setText("" + point1);
     }
 }
