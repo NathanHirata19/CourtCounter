7255ee9460a1de125d73cd0542d12a2bb82a4956
NaThAn H <stuff@whatever.com>
2016-11-09 14:00:01 -0800

    sound buttons alot of stuf

--- app/src/main/java/com/academy/app/courtcounter/MainActivity.java
+++ app/src/main/java/com/academy/app/courtcounter/MainActivity.java
@@ -1,6 +1,7 @@
 package com.academy.app.courtcounter;
 
 import android.content.DialogInterface;
+import android.media.MediaPlayer;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,16 +12,24 @@ public class MainActivity extends AppCompatActivity {
 
     AlertDialog gameOverAlert;
 
+    MediaPlayer mediaPlayer;
 
     private TextView point1View;
     private int point1;
     private TextView point2View;
     private int point2;
 
+    private TextView team1View;
+    private String stringteam1;
+    private TextView team2View;
+    private String stringteam2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         gameOverAlert= new AlertDialog.Builder(MainActivity.this).create();
+
+        mediaPlayer = MediaPlayer.create(this,R.raw.mgsound);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -33,39 +42,50 @@ public class MainActivity extends AppCompatActivity {
         point2=0;
         point2View.setText("0");
 
+        team1View = (TextView)findViewById(R.id.team1);
+        stringteam1="Team billy bob wins";
+        team2View = (TextView)findViewById(R.id.team2);
+        stringteam2="Team joreyjock wins";
+
         }
 
     public void clickpoint1(View view){
         point1++;
-        point1View.setText("" + point1);}
+        point1View.setText("" + point1);
+        mediaPlayer.start();
+    }
 
     public void clickpoint2(View view){
         point2++;
-        point1View.setText("" + point2);}
-
-    public void clickpoint4(View view){
-        point2--;
         point2View.setText("" + point2);
+        mediaPlayer.start();
     }
 
     public void clickpoint3(View view){
         point1--;
         point1View.setText("" + point1);
+        mediaPlayer.start();
+    }
+
+    public void clickpoint4(View view){
+        point2--;
+        point2View.setText("" + point2);
+        mediaPlayer.start();
     }
 
     public void clickgameover(View view){
+        mediaPlayer.start();
         if (point1 > point2) {
-            gameOverAlert.setMessage("Team 1 wins");
+            gameOverAlert.setMessage(stringteam1);
         }
         else if (point1 < point2) {
-            gameOverAlert.setMessage("Team 2 wins");
+            gameOverAlert.setMessage(stringteam2);
         }
         else {
             gameOverAlert.setMessage("It Is a Tie");
         }
 
         gameOverAlert.setTitle("Game Over");
-        gameOverAlert.setMessage("Message Text");
         gameOverAlert.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
